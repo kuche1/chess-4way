260e9e41006e13c878e0772f7208b224d869c552
kuche1 <znamsiparolata4esno@abv.bg>
2024-08-02 14:01:59 +0300

    update

--- chess-4way.cpp
+++ chess-4way.cpp
@@ -6,6 +6,7 @@
 ///
 
 #include <iostream>
+#include <vector>
 
 ///
 //////
@@ -47,6 +48,8 @@ using namespace std;
 #define ICON_QUEEN_BLACK  "♛"
 #define ICON_KING_BLACK   "♚"
 
+#define ICON_WARNING "⚠"
+
 ///
 //////
 /////////// class: piece
@@ -57,7 +60,64 @@ class Piece{
 
     public:
 
-        char * icon;
+        string icon = ICON_WARNING;
+
+        int forward_y = 0;
+        // int forward_x = 0; // this only makes sense in 4way chess
+
+};
+
+///
+//////
+/////////// class: tile
+//////
+///
+
+class Tile{
+
+    public:
+
+        Piece * piece = nullptr;
+
+        // horizontal/vertical
+        Tile * neighbour_up = nullptr;
+        Tile * neighbour_down = nullptr;
+        Tile * neighbour_left = nullptr;
+        Tile * neighbour_right = nullptr;
+        // diagonal
+        Tile * neighbour_upleft = nullptr;
+        Tile * neighbour_upright = nullptr;
+        Tile * neighbour_downleft = nullptr;
+        Tile * neighbour_downright = nullptr;
+
+        int y;
+        int x;
+
+};
+
+///
+//////
+/////////// class: board
+//////
+///
+
+class Board{
+
+    public:
+
+        vector<Tile *> tiles = {};
+
+        Tile * get_tile_at(int y, int x){
+
+            size_t idx = y * 8 + x;
+
+            if(idx >= tiles.size()){
+                return nullptr;
+            }
+
+            return tiles[idx];
+
+        }
 
 };
 
@@ -69,7 +129,108 @@ class Piece{
 
 int main(){
 
-    DBG(ICON_PAWN_BLACK);
+    Board * board = new Board;
+
+    for(int y=0; y<8; ++y){
+        for(int x=0; x<8; ++x){
+
+            Tile * tile = new Tile{
+                .y = y,
+                .x = x,
+            };
+
+            board->tiles.push_back(tile);
+
+        }
+    }
+
+    // connect neighbours
+    
+    for(Tile * tile : board->tiles){
+
+        { // up
+            int nei_up_y = tile->y - 1;
+            int nei_up_x = tile->x;
+            Tile * nei = board->get_tile_at(nei_up_y, nei_up_x);
+            if(nei){
+                tile->neighbour_up = nei;
+            }
+        }
+
+        { // down
+            int nei_up_y = tile->y + 1;
+            int nei_up_x = tile->x;
+            Tile * nei = board->get_tile_at(nei_up_y, nei_up_x);
+            if(nei){
+                tile->neighbour_down = nei;
+            }
+        }
+
+        { // left
+            int nei_up_y = tile->y;
+            int nei_up_x = tile->x - 1;
+            Tile * nei = board->get_tile_at(nei_up_y, nei_up_x);
+            if(nei){
+                tile->neighbour_left = nei;
+            }
+        }
+
+        { // right
+            int nei_up_y = tile->y;
+            int nei_up_x = tile->x + 1;
+            Tile * nei = board->get_tile_at(nei_up_y, nei_up_x);
+            if(nei){
+                tile->neighbour_right = nei;
+            }
+        }
+
+        { // up left
+            int nei_up_y = tile->y - 1;
+            int nei_up_x = tile->x - 1;
+            Tile * nei = board->get_tile_at(nei_up_y, nei_up_x);
+            if(nei){
+                tile->neighbour_upleft = nei;
+            }
+        }
+
+        { // up right
+            int nei_up_y = tile->y - 1;
+            int nei_up_x = tile->x + 1;
+            Tile * nei = board->get_tile_at(nei_up_y, nei_up_x);
+            if(nei){
+                tile->neighbour_upright = nei;
+            }
+        }
+
+        { // down left
+            int nei_up_y = tile->y + 1;
+            int nei_up_x = tile->x - 1;
+            Tile * nei = board->get_tile_at(nei_up_y, nei_up_x);
+            if(nei){
+                tile->neighbour_downleft = nei;
+            }
+        }
+
+        { // down right
+            int nei_up_y = tile->y + 1;
+            int nei_up_x = tile->x + 1;
+            Tile * nei = board->get_tile_at(nei_up_y, nei_up_x);
+            if(nei){
+                tile->neighbour_downright = nei;
+            }
+        }
+
+    }
+
+    // Piece * pawn_black_0 = new Piece{
+    //     .icon = ICON_PAWN_BLACK,
+    //     .forward_y = -1,
+    // };
+
+    // DBG(ICON_PAWN_BLACK);
+
+    // DBG(pawn_black_0->icon);
+    // DBG(pawn_black_0->forward_y);
 
     return 0;
 }
