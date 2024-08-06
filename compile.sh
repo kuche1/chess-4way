27b7134a647df6f41a50a052a0154bc4d79a338b
kuche1 <znamsiparolata4esno@abv.bg>
2024-08-06 18:34:48 +0300

    update

--- compile.sh
+++ compile.sh
@@ -8,9 +8,15 @@ HERE=$(readlink -f $(dirname -- "$BASH_SOURCE"))
 
 APP_NAME='chess-4way'
 
+WASM='y'
+
 # compiler
 
-COMPILER='g++'
+if [ "$WASM" == 'n' ]; then
+	COMPILER='g++'
+else
+	COMPILER='em++'
+fi
 
 # compiler flags
 
@@ -19,13 +25,17 @@ FLAGS_STANDARD='-std=c++23'
 FLAGS_COMPILETIME_SAFETY='-Werror -Wextra -Wall -pedantic -Wfatal-errors -Wshadow -Wconversion'
 # those do not affect the runtime performance
 
+FLAGS_REDUCE_COMPILITIME_SAFETY='-Wno-sign-conversion -Wno-implicit-int-conversion -Wno-limited-postlink-optimizations'
+
 FLAGS_RUNTIME_SAFETY=''
 # those do affect the runtime performance
 # example: -fsanitize=undefined
 # -fsanitize=undefined will detect undefined behaviour at runtime (example: signed overflow)
 
-FLAGS_OPTIMISATION='-Ofast -march=native'
-# example: -Ofast -march=native
+FLAGS_OPTIMISATION='-Ofast'
+if [ "$WASM" != 'y' ]; then
+	FLAGS_OPTIMISATION="$FLAGS_OPTIMISATION -march=native"
+fi
 
 FLAGS_MISC='-g -static'
 # note: can't have both -static and -fsanitize=undefined
@@ -33,17 +43,25 @@ FLAGS_MISC='-g -static'
 FLAGS_LIB=''
 # example: -lgmp
 
-FLAGS="$FLAGS_STANDARD $FLAGS_COMPILETIME_SAFETY $FLAGS_RUNTIME_SAFETY $FLAGS_OPTIMISATION $FLAGS_MISC $FLAGS_LIB"
+FLAGS="$FLAGS_STANDARD $FLAGS_COMPILETIME_SAFETY $FLAGS_REDUCE_COMPILITIME_SAFETY $FLAGS_RUNTIME_SAFETY $FLAGS_OPTIMISATION $FLAGS_MISC $FLAGS_LIB"
 
 # set define containing current git commit
 
 DEFINE_COMMIT_ID=$(git -C "$HERE" rev-parse HEAD)
 
+# output file
+
+if [ "$WASM" == 'n' ]; then
+	OUTPUT="$HERE/$APP_NAME"
+else
+	rm -rf "$HERE/wasm-output"
+	mkdir "$HERE/wasm-output"
+	OUTPUT="$HERE/wasm-output/$APP_NAME.html"
+fi
+
 # compile
 
-output="$HERE/$APP_NAME"
-
-$COMPILER $FLAGS -D COMMIT_ID="\"$DEFINE_COMMIT_ID\"" -o "$output" "$HERE/$APP_NAME.cpp"
+$COMPILER $FLAGS -D COMMIT_ID="\"$DEFINE_COMMIT_ID\"" -o "$OUTPUT" "$HERE/$APP_NAME.cpp"
 
 echo "Compiled binary:"
-echo "$output"
+echo "$OUTPUT"
